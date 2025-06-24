fffd2e780773529b3a8972c891d683a2b0210ae0
md limon ali <limonali105@gmail.com>
2025-06-24 15:24:08 +0600

    fixed navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,41 +12,35 @@ $(document).ready(function () {
 
 	$(".ham-menu").click(function (event) {
 		event.stopPropagation();
-		$(".nav-link-wrapper").toggleClass("active");
+		$(".sidebar-wrapper").toggleClass("active");
 		$(".ham-menu").toggleClass("active");
-		$(".nav-link-wrapper-inner").toggleClass("active");
+		$(".sidebar-wrapper-inner").toggleClass("active");
 		$("body").toggleClass("active");
 	});
 
 	$(".mobile-menu-close-window-btn").click(function (event) {
 		event.stopPropagation();
-		$(".nav-link-wrapper").toggleClass("active");
+		$(".sidebar-wrapper").toggleClass("active");
 		$(".ham-menu").toggleClass("active");
-		$(".nav-link-wrapper-inner").toggleClass("active");
+		$(".sidebar-wrapper-inner").toggleClass("active");
 		$("body").toggleClass("active");
 	});
 
 	// mobile-dropdown responsive accordion js start--
-	$(function () {
-		let isMobile = window.matchMedia("only screen and (max-width: 992px)").matches;
-
-		if (isMobile) {
-			$(".dropdown-sub-menu").slideUp();
-			// **..mobile-dropdown-accordion js start..**
-			$('.dropdown-btn1').on('click', function () {
-				$(this).next(".dropdown-sub-menu1").slideToggle();
-				$(this).toggleClass('active');
-			});
-			// **..mobile-dropdown-accordion js end..**
-
-			// **..mobile-dropdown-accordion js start..**
-			$('.dropdown-btn2').on('click', function () {
-				$(this).next(".dropdown-sub-menu2").slideToggle();
-				$(this).toggleClass('active');
-			});
-			// **..mobile-dropdown-accordion js end..**
-		}
+	$(".dropdown-sub-menu").slideUp();
+	// **..mobile-dropdown-accordion js start..**
+	$('.dropdown-btn1').on('click', function () {
+		$(this).next(".dropdown-sub-menu1").slideToggle();
+		$(this).toggleClass('active');
 	});
+	// **..mobile-dropdown-accordion js end..**
+
+	// **..mobile-dropdown-accordion js start..**
+	$('.dropdown-btn2').on('click', function () {
+		$(this).next(".dropdown-sub-menu2").slideToggle();
+		$(this).toggleClass('active');
+	});
+	// **..mobile-dropdown-accordion js end..**
 	// mobile-dropdown responsive accordion js end--
 
 	$(".search-bar-open-btn").click(function (event) {
